8ded1ba115db3a709f85eed403df5b124c81282c
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-02-27 08:33:40 +0100

    Increase attackstats for better testing

--- src/Charakterersteller.java
+++ src/Charakterersteller.java
@@ -1,17 +1,17 @@
 public class Charakterersteller {
     public Krieger createKrieger() {
-        return new Krieger("Keule", 100, 1, false, 5, 20);
+        return new Krieger("Keule", 100, 1, false, 20, 20);
     }
 
     public Magier createMagier() {
-        return new Magier("Bimsala", 100, 1, false, 5, 50);
+        return new Magier("Bimsala", 100, 1, false, 20, 50);
     }
 
     public Spaeher createSpaeher() {
-        return new Spaeher("Augä", 100, 1, false, 5, 50);
+        return new Spaeher("Augä", 100, 1, false, 20, 50);
     }
 
     public Monster createMonster() {
-        return new Monster("Golfwolf", 100, 1, false, 5);
+        return new Monster("Golfwolf", 100, 1, false, 20);
     }
 }
