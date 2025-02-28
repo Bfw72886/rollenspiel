3ec0010d11a75438bd3acbd396a9981c5c5ec449
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-02-28 11:05:16 +0100

    Integrate database and create method to read content of it

--- src/Charakterersteller.java
+++ src/Charakterersteller.java
@@ -1,4 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Charakterersteller {
+    private ArrayList<Monster> monsters = new ArrayList<>();
+
+    public Charakterersteller() {
+        monsters = readMonsters(connect());
+    }
+
     public Krieger createKrieger() {
         return new Krieger("Keule", 100, 1, false, 20, 20);
     }
@@ -11,7 +22,45 @@ public class Charakterersteller {
         return new Spaeher("Augä", 100, 1, false, 20, 50);
     }
 
-    public Monster createMonster() {
-        return new Monster("Golfwolf", 100, 1, false, 20);
+    public ArrayList<Monster> getMonsters() {
+        return monsters;
+    }
+
+    private ArrayList<Monster> readMonsters(Connection conn) {
+        ArrayList<Monster> monsterFromDB = new ArrayList<>();
+        var sql = "SELECT * FROM Monster";
+
+        try (var stmt = conn.createStatement();
+             var rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                monsterFromDB.add(new Monster(
+                        rs.getString("name"),
+                        rs.getInt("gesundheit"),
+                        rs.getInt("level"),
+                        rs.getBoolean("imKampf"),
+                        rs.getInt("angriffsWert")
+                ));
+            }
+            System.out.println("Arraylist<Monster> successfully read from DB");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return monsterFromDB;
+    }
+
+    private Connection connect() {
+        var url = "jdbc:sqlite:rollenspieldb.db";
+
+        try {
+            var conn = DriverManager.getConnection(url);
+            System.out.println("DB connection successful.");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
