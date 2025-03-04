951e6dfca73fc21490521a478e50d1ab4df68e99
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-04 13:05:47 +0100

    Delete empty constructor

--- src/logic/DBreader.java
+++ src/logic/DBreader.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 
 public class DBreader {
 
-    public DBreader() {
-    }
-
     public ArrayList<Monster> readMonsters(int difficulty) {
         Connection conn = connect();
         ArrayList<Monster> monsterFromDB = new ArrayList<>();
