e5e0eacb42e19c2344edcc3986b51d3bab9424dd
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-11 10:59:57 +0100

    Add explanation of the game

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
+import charakters.Charakter;
 import charakters.Charakterersteller;
-import charakters.Krieger;
 import logic.Dungeon;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        Diese drei Befehle ermöglichen dir den Zutritt in den Dungeon.
+        Das gesamte Spiel findet in der Konsole statt.
+        Die Steuerung findet mit den Tasten:
+        [j][k][l] und <Enter>
+        statt. Du kannst es also sogar mit einer Hand spielen.
+        Viel Spaß!
+         */
         Charakterersteller charakterersteller = new Charakterersteller();
-
-        Krieger krieger = charakterersteller.createKrieger();
-
-        Dungeon dungeon = new Dungeon(krieger);
-        dungeon.start();
+        Charakter deinCharakter = charakterersteller.createYourCharakter();
+        new Dungeon(deinCharakter);
     }
 }
